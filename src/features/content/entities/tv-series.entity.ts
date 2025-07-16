78b12ade722f6b6fc45f0a5aaadb3f272942fa52
Minseo Park <hightuv@gmail.com>
2025-07-16 23:13:34 +0900

    Fix: 한국어 제목 존재 여부가 올바르지 않게 저장되고 있던 것을 수정

--- src/features/content/entities/tv-series.entity.ts
+++ src/features/content/entities/tv-series.entity.ts
@@ -85,7 +85,7 @@ export class TvSeries extends Content {
       this.englishTitle = englishTranslation?.data?.title ?? null;
 
       const koreanTranslation = translations.find((t) => t.iso_639_1 === 'ko');
-      this.hasKoreanTitle = !!koreanTranslation?.data?.title;
+      this.hasKoreanTitle = !!koreanTranslation;
     } else {
       this.hasKoreanTitle = false;
     }
